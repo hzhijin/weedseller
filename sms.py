c0807f8d448b7ae8472cab4a3ea7a4ae18484bb7
boyang <21258210@qq.com>
2022-02-05 20:21:02 -0800

    Add files via upload

--- sms.py
+++ sms.py
@@ -1,115 +1,84 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jan 18 21:56:19 2022
+Created on Tue Jan 18 22:24:20 2022
 
-@author: Billy
+@author: Golden Mars
 """
+
 
 'https://www.liquisearch.com/list_of_sms_gateways'
-
-"""
-AT&T: [number]@txt.att.net
-Sprint: [number]@messaging.sprintpcs.com or [number]@pm.sprint.com
-T-Mobile: [number]@tmomail.net
-Verizon: [number]@vtext.com
-Boost Mobile: [number]@myboostmobile.com
-Cricket: [number]@sms.mycricket.com
-Metro PCS: [number]@mymetropcs.com
-Tracfone: [number]@mmst5.tracfone.com
-U.S. Cellular: [number]@email.uscc.net
-Virgin Mobile: [number]@vmobl.com
-
-"""
 
 import smtplib 
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+import time
+import pandas as pd
+from tqdm import tqdm
 
-"""
-def sendSMS(_from,_pas,_to,_object,_body):
+def sendSMS(_from,_pas,_to,_subject,_body):
 	smtp = "smtp.gmail.com" 
 	port = 587
 	server = smtplib.SMTP(smtp,port)
 	server.starttls()
 	server.login(_from,_pas)
-	
 	msg = MIMEMultipart()
 	msg['From'] = _from
 	msg['To'] = _to
-	
-	msg['Subject'] = _object
-	msg.attach(MIMEText(_body, 'plain'))
+	msg['Subject'] = _subject
+	body = _body
+# 	msg.attach(MIMEText(body, 'plain'))
+	msg.attach(MIMEText(body))
 	sms = msg.as_string()
 	server.sendmail(_from,_to,sms)
 	server.quit()
-	
-gateways = ['@txt.att.net',
+
+gateways = ['@mms.att.net',
 			'@messaging.sprintpcs.com',
 			'@tmomail.net',
-			'vtext.com',
+			'@vtext.com',
 			'@sms.mycricket.com',]
 
-senders = ["okcandylau@gmail.com",
-		   "okcandylau2@gmail.com",
-		   "okcandylau3@gmail.com",
-		   "okcandylau4@gmail.com",
-		   "okcandylau5@gmail.com",
-		   "okcandylau6@gmail.com",
-		   "okcandylau7@gmail.com",
-		   "okcandylau8@gmail.com",
+
+senders = ["usseller0001@gmail.com",
+ 		   "usseller0002@gmail.com",
+		   "usseller0003@gmail.com",
+		   "usseller0004@gmail.com",
+		   "usseller0005@gmail.com",
+		   "usseller0006@gmail.com",
+		   "usseller0007@gmail.com",
+		   "usseller0008@gmail.com",
+           "usseller0009@gmail.com",
+           "usseller0010@gmail.com",
+           "usseller0011@gmail.com",
 		   ]
-pas = 'guo654321'
+_pas = "spring1708"
+
+df = pd.read_excel('omma_growers_list.xlsx')
+df['phone'] = df.phone.apply(str)
+df['phone'] = df.phone.str.replace('(','')
+df['phone'] = df.phone.str.replace(')','')
+df['phone'] = df.phone.str.replace(' ','')
+df['phone'] = df.phone.str.replace('-','')
+
+
+_subject = 'Phantom grow lights available'
+with open('mail.txt') as f:
+	_body = f.read()
+
+senderIndex = 10
+_from = senders[senderIndex]
+
+# for i in tqdm(range(senderIndex * int(len(df)/11)  , (senderIndex+1) * int(len(df)/11))):
+for i in tqdm(range(senderIndex * int(len(df)/11) , len(df))):
+# for i in range(1):
+	for j in gateways:
+		_to = df.iloc[i]['phone'][-10:] + j
+		print(_to)
+		try:
+			sendSMS(_from,_pas,_to,_subject,_body)
+		except Exception as e:
+			print(e)
+			time.sleep(1200)
+		time.sleep(60)
 	
-_from = "okcandylau@gmail.com"
-_pas = "guo654321"
-_to = '4054122710@txt.att.net'
-_object = 'This is the TITLE\n'
-_body = 'This is the BODY\n'
-
-# sendSMS(_from,_pas,_to,_object,_body)
-
-smtp = "smtp.gmail.com" 
-port = 587
-server = smtplib.SMTP(smtp,port)
-server.starttls()
-server.login(_from,_pas)
-
-msg = MIMEMultipart()
-msg['From'] = _from
-msg['To'] = _to
-
-msg['Subject'] = _object
-msg.attach(MIMEText(_body, 'plain'))
-sms = msg.as_string()
-server.sendmail(_from,_to,sms)
-server.quit()"""
 	
-
-email = "okcandylau6@gmail.com"
-pas = "guo654321"
-sms_gateway = '4054122710@txt.att.net'
-
-# def sendSMS(email,pas,sms_gateway):
-	
-smtp = "smtp.gmail.com" 
-port = 587
-server = smtplib.SMTP(smtp,port)
-server.starttls()
-server.login(email,pas)
-
-msg = MIMEMultipart()
-msg['From'] = email
-msg['To'] = sms_gateway
-msg['Subject'] = "You can insert anything\n"
-body = "You can insert message here aaa\n"
-msg.attach(MIMEText(body, 'plain'))
-sms = msg.as_string()
-server.sendmail(email,sms_gateway,sms)
-server.quit()
-
-	
-
-
-
-
-# sendSMS(email,pas,sms_gateway)
